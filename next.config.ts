e635a534ea9c20a7a61fa5ef344226a3d50d3947
Kolenchits Sergy <Kolenchitssi@gmail.com>
2025-04-08 07:53:57 +0300

    feat: edit config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  // pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'], // if we used mdx
   /* config options here */
 };
 
